c66a91c207dd26b1f7eab24636fcae48103cd7a8
Jiefeng <xerath7d0@gmail.com>
2024-01-12 16:05:21 +0800

    dapated to solc 0.8x and formated

--- src/levels/FalloutFactory.sol
+++ src/levels/FalloutFactory.sol
@@ -1,20 +1,24 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.13;
 
-import './base/Level-06.sol';
-import './Fallout.sol';
+import "./base/Level.sol";
+import "./Fallout.sol";
 
 contract FalloutFactory is Level {
+    function createInstance(
+        address _player
+    ) public payable override returns (address) {
+        _player;
+        Fallout instance = new Fallout();
+        return address(instance);
+    }
 
-  function createInstance(address _player) override public payable returns (address) {
-    _player;
-    Fallout instance = new Fallout();
-    return address(instance);
-  }
-
-  function validateInstance(address payable _instance, address _player) override public returns (bool) {
-    Fallout instance = Fallout(_instance);
-    return instance.owner() == _player;
-  }
+    function validateInstance(
+        address payable _instance,
+        address _player
+    ) public view override returns (bool) {
+        Fallout instance = Fallout(_instance);
+        return instance.owner() == _player;
+    }
 }
