34c77ed6b598aa1a9d7cd7614931ca469a9f4d21
Jiefeng Li <xerath7d0@gmail.com>
2024-01-17 20:13:38 +0800

    adapted to solc 0.8x and formated for level 10

--- src/levels/Reentrance.sol
+++ src/levels/Reentrance.sol
@@ -1,30 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
-
-import 'openzeppelin-contracts-06/math/SafeMath.sol';
+pragma solidity ^0.8.13;
 
 contract Reentrance {
-  
-  using SafeMath for uint256;
-  mapping(address => uint) public balances;
+    mapping(address => uint) public balances;
 
-  function donate(address _to) public payable {
-    balances[_to] = balances[_to].add(msg.value);
-  }
-
-  function balanceOf(address _who) public view returns (uint balance) {
-    return balances[_who];
-  }
-
-  function withdraw(uint _amount) public {
-    if(balances[msg.sender] >= _amount) {
-      (bool result,) = msg.sender.call{value:_amount}("");
-      if(result) {
-        _amount;
-      }
-      balances[msg.sender] -= _amount;
+    function donate(address _to) public payable {
+        balances[_to] = balances[_to] + msg.value;
     }
-  }
 
-  receive() external payable {}
+    function balanceOf(address _who) public view returns (uint balance) {
+        return balances[_who];
+    }
+
+    function withdraw(uint _amount) public {
+        if (balances[msg.sender] >= _amount) {
+            (bool result, ) = msg.sender.call{value: _amount}("");
+            if (result) {
+                _amount;
+            }
+            balances[msg.sender] -= _amount;
+        }
+    }
+
+    receive() external payable {}
 }
--- src/levels/ReentranceFactory.sol
+++ src/levels/ReentranceFactory.sol
@@ -1,28 +1,32 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.13;
 
-import './base/Level-06.sol';
-import './Reentrance.sol';
+import "./base/Level.sol";
+import "./Reentrance.sol";
 
 contract ReentranceFactory is Level {
+    uint public insertCoin = 0.001 ether;
 
-  uint public insertCoin = 0.001 ether;
+    function createInstance(
+        address _player
+    ) public payable override returns (address) {
+        _player;
+        require(msg.value >= insertCoin);
+        Reentrance instance = new Reentrance();
+        require(address(this).balance >= insertCoin);
+        payable(address(instance)).transfer(insertCoin);
+        return address(instance);
+    }
 
-  function createInstance(address _player) override public payable returns (address) {
-    _player;
-    require(msg.value >= insertCoin);
-    Reentrance instance = new Reentrance();
-    require(address(this).balance >= insertCoin);
-    address(instance).transfer(insertCoin);
-    return address(instance);
-  }
+    function validateInstance(
+        address payable _instance,
+        address _player
+    ) public view override returns (bool) {
+        _player;
+        Reentrance instance = Reentrance(_instance);
+        return address(instance).balance == 0;
+    }
 
-  function validateInstance(address payable _instance, address _player) override public returns (bool) {
-    _player;
-    Reentrance instance = Reentrance(_instance);
-    return address(instance).balance == 0;
-  }
-
-  receive() external payable {}
+    receive() external payable {}
 }
