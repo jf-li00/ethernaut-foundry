a4e350b63fdf73594d11b0e6f742ec5eba0fdee2
Jiefeng Li <xerath7d0@gmail.com>
2024-01-17 18:32:30 +0800

    adapted to solc 0.8x for level 5

--- src/levels/Token.sol
+++ src/levels/Token.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.13;
 
 contract Token {
+    mapping(address => uint) balances;
+    uint public totalSupply;
 
-  mapping(address => uint) balances;
-  uint public totalSupply;
+    constructor(uint _initialSupply) {
+        balances[msg.sender] = totalSupply = _initialSupply;
+    }
 
-  constructor(uint _initialSupply) public {
-    balances[msg.sender] = totalSupply = _initialSupply;
-  }
+    function transfer(address _to, uint _value) public returns (bool) {
+        unchecked {
+            require(balances[msg.sender] - _value >= 0);
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            return true;
+        }
+    }
 
-  function transfer(address _to, uint _value) public returns (bool) {
-    require(balances[msg.sender] - _value >= 0);
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    return true;
-  }
-
-  function balanceOf(address _owner) public view returns (uint balance) {
-    return balances[_owner];
-  }
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner];
+    }
 }
--- src/levels/TokenFactory.sol
+++ src/levels/TokenFactory.sol
@@ -1,23 +1,27 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.13;
 
-import './base/Level-06.sol';
-import './Token.sol';
+import "./base/Level.sol";
+import "./Token.sol";
 
 contract TokenFactory is Level {
+    uint supply = 21000000;
+    uint playerSupply = 20;
 
-  uint supply = 21000000;
-  uint playerSupply = 20;
+    function createInstance(
+        address _player
+    ) public payable override returns (address) {
+        Token token = new Token(supply);
+        token.transfer(_player, playerSupply);
+        return address(token);
+    }
 
-  function createInstance(address _player) override public payable returns (address) {
-    Token token = new Token(supply);
-    token.transfer(_player, playerSupply);
-    return address(token);
-  }
-
-  function validateInstance(address payable _instance, address _player) override public returns (bool) {
-    Token token = Token(_instance);
-    return token.balanceOf(_player) > playerSupply;
-  }
+    function validateInstance(
+        address payable _instance,
+        address _player
+    ) public override returns (bool) {
+        Token token = Token(_instance);
+        return token.balanceOf(_player) > playerSupply;
+    }
 }
