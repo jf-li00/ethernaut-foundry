474113851d7d338425ddba1d0a4f00953dd11fe5
Jiefeng Li <xerath7d0@gmail.com>
2024-01-17 21:09:25 +0800

    added test framework for level 11

---
+++ test/Level11-Elevator.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "@levels/Elevator.sol";
+import "@levels/ElevatorFactory.sol";
+import "@attacks/Level11-Elevator-Attacker.sol";
+
+contract POC is Test {
+    Elevator public victim;
+    Attacker public attacker;
+
+    ElevatorFactory public factory;
+
+    function setUp() public {
+        factory = new ElevatorFactory();
+        address victim_addr = factory.createInstance(address(attacker));
+        victim = Elevator(victim_addr);
+        attacker = new Attacker(address(victim));
+
+        vm.label(address(victim), "victim_contract");
+        vm.label(address(attacker), "attacker_contract");
+
+        vm.deal(address(victim), 10 ether);
+        vm.deal(address(attacker), 1 ether);
+    }
+
+    function test_exploit() public {
+        console.log(
+            unicode"\n   📚📚 All things reentrancy: basic exploitation\n"
+        );
+        console.log("--------------------------------------------------------");
+
+        console.log(unicode"\n\t💥💥💥💥 EXPLOITING... 💥💥💥💥\n");
+
+        attacker.exploit();
+
+        // Conditions to fullfill
+        assert(
+            factory.validateInstance(
+                payable(address(victim)),
+                address(attacker)
+            )
+        );
+
+        console.log("--------------------------------------------------------");
+    }
+
+    function toEth(uint256 _wei) internal pure returns (string memory) {
+        string memory eth = vm.toString(_wei / 1 ether);
+        string memory decs = vm.toString(_wei % 1 ether);
+
+        string memory result = string.concat(string.concat(eth, "."), decs);
+
+        return result;
+    }
+}
